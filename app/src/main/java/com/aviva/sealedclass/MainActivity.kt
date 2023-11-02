24285c785697a61acf3a72bfa68dee821b037b78
Javier Gonzalez <developer.jgv@gmail.com>
2023-11-02 14:51:43 -0600

    Creando Modelos y Pasando Información

--- app/src/main/java/com/aviva/sealedclass/MainActivity.kt
+++ app/src/main/java/com/aviva/sealedclass/MainActivity.kt
@@ -2,24 +2,34 @@ package com.aviva.sealedclass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aviva.seaedclass.domain.GameError
+import com.aviva.seaedclass.domain.GameError.*
 import com.aviva.seaedclass.domain.GameModel
 
 class MainActivity : AppCompatActivity() {
 
     val gameList = listOf<GameModel>(
-        GameModel("Mario Bros", "0001", "Rayado"),
-        GameModel("Halo", "0002", "Sucio"),
-        GameModel("Isaac", "0003", "")
+        GameModel("Mario Bros", "0001", noError),
+        GameModel("Halo", "0002", sucioError),
+        GameModel("Isaac", "0003", rayadoError),
+        GameModel("Pacman", "0003", versionError("1.0.1"))
     )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         gameList.forEach {gameModel ->
             when(gameModel.error){
-                "Rayado" -> llevarAlSoporteTecnico()
-                "Sucio" -> llamarAlSoporteTecnico()
-                "" -> vender()
-                else  -> {}
+                noError -> vender()
+                rayadoError -> llamarAlSoporteTecnico()
+                sucioError -> llevarAlSoporteTecnico()
+                congeladoError -> eliminarJuego()
+                /**
+                 *  No se recomienda poner el else en estos casos para siempre tener
+                 *  una buena respuesta cuando cae al caso
+                 *
+                 */
+                //else -> {}
+                is versionError -> consultarVersion(gameModel.error.version)
             }
         }
     }
@@ -31,4 +41,6 @@ class MainActivity : AppCompatActivity() {
     fun vender(){}
 
     fun eliminarJuego(){}
+
+    fun consultarVersion(version:String){}
 }
