6f062412f0e77beace40b32ddf835918712af2ee
Javier Gonzalez <developer.jgv@gmail.com>
2023-11-02 14:30:13 -0600

    Creando Proyecto

--- app/src/main/java/com/aviva/sealedclass/MainActivity.kt
+++ app/src/main/java/com/aviva/sealedclass/MainActivity.kt
@@ -2,10 +2,33 @@ package com.aviva.sealedclass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aviva.seaedclass.domain.GameModel
 
 class MainActivity : AppCompatActivity() {
+
+    val gameList = listOf<GameModel>(
+        GameModel("Mario Bros", "0001", "Rayado"),
+        GameModel("Halo", "0002", "Sucio"),
+        GameModel("Isaac", "0003", "")
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        gameList.forEach {gameModel ->
+            when(gameModel.error){
+                "Rayado" -> llevarAlSoporteTecnico()
+                "Sucio" -> llamarAlSoporteTecnico()
+                "" -> vender()
+                else  -> {}
+            }
+        }
     }
+
+    fun  llevarAlSoporteTecnico(){}
+
+    fun llamarAlSoporteTecnico(){}
+
+    fun vender(){}
+
+    fun eliminarJuego(){}
 }
---
+++ app/src/main/java/com/aviva/sealedclass/domain/GameModel.kt
@@ -0,0 +1,8 @@
+package com.aviva.seaedclass.domain
+
+
+data class GameModel(
+    val title:String,
+    val serialNumber: String,
+    val error: String
+)
